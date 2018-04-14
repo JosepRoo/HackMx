abe70108251e751ea41e178d4e4d883580811923
ricardo <ricardo.strifeff7@gmail.com>
2018-04-14 15:06:23 -0500

    ini

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 """
 The following environment variables are suggested to be set.
 If the database URLs are not set, the application will default
@@ -13,28 +14,23 @@ FLASK_CONFIG (will be development, testing, production or default)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	STATIC_FOLDER = os.path.join(os.pardir, 'static/dist')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    STATIC_FOLDER = os.path.join(os.pardir, 'static/dist')
+
 
 class DevelopmentConfig(Config):
-	DEBUG = True
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_here'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_here'
 
-class TestingConfig (Config):
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
-class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+class TestingConfig(Config):
+    TESTING = True
+
 
 config = {
-	'development': DevelopmentConfig,
-	'testing':	TestingConfig,
-	'production': ProductionConfig,
-	'default': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
 }
--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 import os
 from app import create_app
 from flask_script import Manager
-from flask import Flask
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-#app = Flask(__name__)
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()

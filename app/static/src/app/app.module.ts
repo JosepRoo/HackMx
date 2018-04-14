6e8e9479b1c4bd804de194c2e5ab91296c550569
Josep Romagosa <josep_roo@live.com>
2018-04-14 17:31:31 -0500

    Primera Version Front

--- app/static/src/app/app.module.ts
+++ app/static/src/app/app.module.ts
@@ -1,29 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import 'hammerjs';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import 'hammerjs';
 
+import { FuseModule } from '@fuse/fuse.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import { fuseConfig } from './fuse-config';
+
+import { AppComponent } from './app.component';
+import { FuseMainModule } from './main/main.module';
+import { FuseSampleModule } from './main/content/sample/sample.module';
 
 const appRoutes: Routes = [
     {
         path      : '**',
-        //comonent: [YourImportedComponentClass]
-    },
-
+        redirectTo: 'sample'
+    }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    declarations: [
+        AppComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+        TranslateModule.forRoot(),
 
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        // Fuse Main and Shared modules
+        FuseModule.forRoot(fuseConfig),
+        FuseSharedModule,
+        FuseMainModule,
+        FuseSampleModule
+    ],
+    bootstrap   : [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule
+{
+}

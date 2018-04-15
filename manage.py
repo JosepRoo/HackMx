31f78fc144142541ca1fcb43ac5032763cd72dbd
ricardo <ricardo.strifeff7@gmail.com>
2018-04-15 09:46:54 -0500

    finale

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@ from app import create_app
 from flask_script import Manager, Server
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#server = Server(host="159.89.150.55", port=9000)
+server = Server(host="159.89.150.55", port=9000)
 manager = Manager(app)
-#manager.add_command("runserver", server)
+manager.add_command("runserver", server)
 
 if __name__ == '__main__':
     manager.run()

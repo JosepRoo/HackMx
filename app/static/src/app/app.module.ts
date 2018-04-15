9eebad1f2436370b717ae4ea987ed100689813da
Josep Romagosa <josep_roo@live.com>
2018-04-15 03:55:21 -0500

    dashboard

--- app/static/src/app/app.module.ts
+++ app/static/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { TranslateModule } from '@ngx-translate/core';
 import 'hammerjs';
 
@@ -12,13 +13,43 @@ import { FuseSharedModule } from '@fuse/shared.module';
 import { fuseConfig } from './fuse-config';
 
 import { AppComponent } from './app.component';
+import { FuseFakeDbService } from './fuse-fake-db/fuse-fake-db.service';
 import { FuseMainModule } from './main/main.module';
-import { FuseSampleModule } from './main/content/sample/sample.module';
+import { AppStoreModule } from './store/store.module';
 
 const appRoutes: Routes = [
     {
+        path        : 'apps',
+        loadChildren: './main/content/apps/apps.module#FuseAppsModule'
+    },
+    {
+        path        : 'pages',
+        loadChildren: './main/content/pages/pages.module#FusePagesModule'
+    },
+    {
+        path        : 'ui',
+        loadChildren: './main/content/ui/ui.module#FuseUIModule'
+    },
+    {
+        path        : 'services',
+        loadChildren: './main/content/services/services.module#FuseServicesModule'
+    },
+    {
+        path        : 'components',
+        loadChildren: './main/content/components/components.module#FuseComponentsModule'
+    },
+    {
+        path        : 'components-third-party',
+        loadChildren: './main/content/components-third-party/components-third-party.module#FuseComponentsThirdPartyModule'
+    },
+    {
         path      : '**',
-        redirectTo: 'sample'
+        redirectTo: 'apps/dashboards/analytics'
+    }
+    ,
+    {
+        path      : '*',
+        redirectTo: 'apps/dashboards/analytics'
     }
 ];
 
@@ -31,13 +62,19 @@ const appRoutes: Routes = [
         BrowserAnimationsModule,
         HttpClientModule,
         RouterModule.forRoot(appRoutes),
+
         TranslateModule.forRoot(),
+        InMemoryWebApiModule.forRoot(FuseFakeDbService, {
+            delay             : 0,
+            passThruUnknownUrl: true
+        }),
 
         // Fuse Main and Shared modules
         FuseModule.forRoot(fuseConfig),
         FuseSharedModule,
-        FuseMainModule,
-        FuseSampleModule
+
+        AppStoreModule,
+        FuseMainModule
     ],
     bootstrap   : [
         AppComponent

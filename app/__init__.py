9de36c585f4896611d54ee9ee1f23da2dfe1255f
ricardo <ricardo.strifeff7@gmail.com>
2018-04-14 22:32:49 -0500

    misc

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,12 @@ def create_app(config_name):
     from .default import default as default_blueprint
     app.register_blueprint(default_blueprint)
 
+    from app.models.users.views import user_blueprint
+    app.register_blueprint(user_blueprint)
+
+    from app.models.vehicles.views import vehicle_blueprint
+    app.register_blueprint(vehicle_blueprint)
+
     # Initialize any extensions we are using
     bootstrap.init_app(app)
     Database.initialize()
--- app/models/vehicles/views.py
+++ app/models/vehicles/views.py
@@ -2,9 +2,9 @@ from flask import Blueprint, request, session, jsonify
 from app.common.response import Response
 from app.models.vehicles.vehicle import Vehicle
 
-user_blueprint = Blueprint('vehicles', __name__)
+vehicle_blueprint = Blueprint('vehicles', __name__)
 
-@user_blueprint.route('/get_vehicle/<string:vehicle_id>', methods=['GET'])
+@vehicle_blueprint.route('/get_vehicle/<string:vehicle_id>', methods=['GET'])
 def get_vehicle(vehicle_id=None):
     vehicles = None
     if vehicle_id is None:

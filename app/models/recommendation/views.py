3785257fe8cb3ba7816de6df1b23c119e774c814
ricardo <ricardo.strifeff7@gmail.com>
2018-04-15 09:48:12 -0500

    finale

--- app/models/recommendation/views.py
+++ app/models/recommendation/views.py
@@ -6,7 +6,7 @@ recommendations_blueprint = Blueprint('recommendations', __name__)
 
 @recommendations_blueprint.route('/get', methods=['GET'])
 @recommendations_blueprint.route('/get/<string:user_id>', methods=['GET'])
-def get_by_id(user_id):
+def get_by_id(user_id=None):
     data = Recommendation.get_by_user_id(user_id)
     if data is not None:
         return jsonify(Response(success=True, records=len(data), data=data))
